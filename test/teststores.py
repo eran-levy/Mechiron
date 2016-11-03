43bb7dcb41e670384700f8c2bca5d8ec80293772
eran <mceranlevy@gmail.com>
2016-11-03 18:00:50 +0200

    unit tests

--- test/teststores.py
+++ test/teststores.py
@@ -6,12 +6,12 @@ from mechiron.extractors.pricesgzextractor import PricesExtractor
 class MyTestCase(unittest.TestCase):
 
     def test_extractstores(self):
-        extractor = StoreExtractor("/home/eran/Downloads/mechiron-downloads/2016-10-16_07_48_06")
+        extractor = StoreExtractor("/home/eran/Downloads/mechiron-downloads/2016-10-11_10_18_55")
         extractor.extract_stores_to_csv()
         # self.assertEqual(True, False)
 
     def test_extractprices(self):
-        prices_extractor = PricesExtractor("/home/eran/Downloads/mechiron-downloads/2016-10-16_07_48_06")
+        prices_extractor = PricesExtractor("/home/eran/Downloads/mechiron-downloads/2016-10-11_10_18_55")
         prices_extractor.process_gz_files()
 
 if __name__ == '__main__':
